89bd878d631815381e8b0bd8b6182ac9d4fc4a0e
Fastiman <5336643+fastiman@users.noreply.github.com>
2023-03-18 16:22:31 +0300

    process payments using stripe

--- main/core/models.py
+++ main/core/models.py
@@ -80,3 +80,14 @@ class CheckoutAddress(models.Model):
 
     def __str__(self):
         return self.user.username
+
+
+class Payment(models.Model):
+    stripe_id = models.CharField(max_length=50)
+    user = models.ForeignKey(settings.AUTH_USER_MODEL,
+                             on_delete=models.SET_NULL, blank=True, null=True)
+    amount = models.FloatField()
+    timestamp = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.user.username
--- main/main/settings.py
+++ main/main/settings.py
@@ -127,3 +127,5 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'assets')
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+STRIPE_KEY = ''
